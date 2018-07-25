81baebf3d91e7f504daf9a9f953828562319732d
Andreas Grimm <a.grimm@bnotk.de>
2018-07-25 19:14:05 +0200

    proof of concept, initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+// created by andreas 2017-07-28
+//
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const fs=require("fs");
+const https=require("https");
+const extend=require("extend");
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED']='0';
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+const _dhcp = require('./lib/dhcp.js');
+const dhcp = new _dhcp();
+const _dns = require('./lib/dns.js');
+const dns = new _dns();
+
+app.route('/features')
+  .get(function (req,res) {
+    res.send('["dhcp","dns"]').end();
+  });
+
+app.route('/version')
+  .get(function(req,res) {
+    res.send('{"version":"1.17.0","modules":{"dns":"1.17.0","dhcp":"1.17.0"}}').end();
+  });
+
+app.route('/dhcp')
+  .get(dhcp.dhcp);
+
+app.route('/dhcp/:network/unused_ip')
+  .get(dhcp.unused_ip);
+
+app.route('/dhcp/:network/:ip')
+  .get(dhcp.reservation_info);
+
+app.route('/dhcp/:network/mac/:mac')
+  .get(dhcp.search_mac)
+  .delete(dhcp.delete_reservation);
+
+app.route('/dhcp/:network/ip/:ip')
+  .get(dhcp.search_ip)
+
+app.route('/dhcp/:network')
+  .post(dhcp.create_reservation);
+
+app.route('/dns')
+  .post(dns.create);
+
+app.route('/dns/:value')
+  .delete(dns.delete);
+
+try {
+  var privateKey  = fs.readFileSync('data/conf/ssl.key', 'utf8');
+  var certificate = fs.readFileSync('data/conf/ssl.pem', 'utf8');
+  var credentials = {key: privateKey, cert: certificate};
+  var httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(9443);
+} catch (e) {
+  app.listen(8080)
+}
+
+process.on('uncaughtException', function (err) {
+    console.error(err.stack);
+    console.log("node NOT exiting...");
+});
