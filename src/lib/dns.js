5e2b4ea0883d75290b187b97dc3b8ca9c8209fa3
Andreas Grimm <a.grimm@bnotk.de>
2019-08-27 15:46:57 +0200

    scope hell

--- src/lib/dns.js
+++ src/lib/dns.js
@@ -8,7 +8,11 @@ var dns = function(dnsservers) {
     var infoblox = new _infoblox();
     var util = new _util();
     var dnsservers = dnsservers;
-    var dnscache = {};
+
+    this.constructor = function() {
+        this.dnscache = {};
+    }
+
     this.create = function(req,res) {
         if(req.body.type==='PTR') {
             if(!util.is_fqdn(req.body.fqdn)||!util.is_ptr(req.body.value)) {
@@ -91,16 +95,16 @@ var dns = function(dnsservers) {
 
   // noop clone of autocreate
     this.autocreate_noop = function(req,res) {
-        console.log(dnscache);
+        console.log(this.dnscache);
         infoblox.request('network','GET',{'comment:~':req.params.networkname},[],function(data) {
             if(data[0] !== undefined) {
                 // CALLBACK HELL!!!
                 // check cache
-                if(dnscache[req.params.value]!==undefined) {
-                  res.send(dnscache[req.params.value]).end();
+                if(this.dnscache[req.params.value]!==undefined) {
+                  res.send(this.dnscache[req.params.value]).end();
                   return;
                 }
-                if(dnsservers!==undefined) { dnshelper.setServers(dnsservers); console.log("Using "+dnsservers+" for resolving..."); }
+                if(this.dnsservers!==undefined) { dnshelper.setServers(dnsservers); console.log("Using "+dnsservers+" for resolving..."); }
                 dnshelper.resolve(req.params.value,function(err,records) {
                     if(records!==undefined) {
                         res.send(records[0]).end();
@@ -116,8 +120,8 @@ var dns = function(dnsservers) {
     }
 
     this.clear_cache = function(req,res) {
-        var msg = {'msg':dnscache.length+' entries deleted from cache'}
-        dnscache.length = 0;
+        var msg = {'msg':this.dnscache.length+' entries deleted from cache'}
+        this.dnscache.length = 0;
         res.status(200).send(JSON.stringify(msg));
     }
 
@@ -127,18 +131,18 @@ var dns = function(dnsservers) {
             if(data[0] !== undefined) {
                 // CALLBACK HELL!!!
                 // check cache
-                if(dnscache[req.params.value]!==undefined) {
-                  res.send(dnscache[req.params.value]).end();
+                if(this.dnscache[req.params.value]!==undefined) {
+                  res.send(this.dnscache[req.params.value]).end();
                   return;
                 }
                 if(dnsservers!==undefined) { dnshelper.setServers(dnsservers); console.log("Using "+dnsservers+" for resolving..."); }
                 dnshelper.resolve(req.params.value,function(err,records) {
                     if(records!==undefined) {
-                        dnscache[req.params.value] = records[0];
+                        this.dnscache[req.params.value] = records[0];
                         res.send(records[0]).end();
                         return;
                     } else {
-                      dnscache[req.params.value] = undefined;
+                      this.dnscache[req.params.value] = undefined;
                         // find unused ip
                         request('http://localhost:8080/dhcp/'+data[0].network.split('/')[0]+'/unused_ip',function(err,response,body) {
                             try {
@@ -150,7 +154,7 @@ var dns = function(dnsservers) {
                                         res.status(500).send('ERR error on reservation');
                                         return;
                                     }
-                                    dnscache[req.params.value]=ip;
+                                    this.dnscache[req.params.value]=ip;
                                     res.status(200).send(ip);
                                 });
                             } catch (e) {
