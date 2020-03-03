619b88e4260a5f148c3513d8542af5946a14d764
Andreas Grimm <a.grimm@bnotk.de>
2020-03-03 11:37:27 +0100

    basic auth just on some paths

--- src/server.js
+++ src/server.js
@@ -17,24 +17,21 @@ const pass = JSON.parse(fs.readFileSync('conf/config.json')).pass;
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED']='0';
 
+function auth(on) {
+  console.log(basicauth);
+  if(basicauth === true && on === true) {
+    var users = {};
+    users[user]=pass;
+    return basicAuth({users:users});
+  } else {
+    console.log('open');
+    return function(req,res,next) { next(); };
+  }
+}
+
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended:true}));
-if(basicauth===true) {
-  var users = {};
-  users[user]=pass;
-  app.use(basicAuth({users: users }));
-} else {
-  app.use(function (req,res,next) {
-    if(allowedips===undefined) next();
-    else {
-      console.log(req.ip);
-      console.log(allowedips.indexOf(req.ip));
-      if(allowedips.indexOf(req.ip)>=0) next();
-      else res.status(401).send('not authorized').end();
-    }
-  });
-}
 
 const _dhcp = require('./lib/dhcp.js');
 const dhcp = new _dhcp();
@@ -42,47 +39,47 @@ const _dns = require('./lib/dns.js');
 const dns = new _dns(dnsservers);
 
 app.route(apipath+'/features')
-  .get(function (req,res) {
+  .get(auth(false),function (req,res) {
     res.send('["dhcp","dns"]').end();
   });
 
 app.route(apipath+'/version')
-  .get(function(req,res) {
+  .get(auth(false),function(req,res) {
     res.send('{"version":"1.20.0","modules":{"dns":"1.20.0","dhcp":"1.20.0"}}').end();
   });
 
 app.route(apipath+'/dhcp')
-  .get(dhcp.dhcp);
+  .get(auth(true),dhcp.dhcp);
 
 app.route(apipath+'/dhcp/:network/unused_ip')
-  .get(dhcp.unused_ip);
+  .get(auth(true),dhcp.unused_ip);
 
 app.route(apipath+'/dhcp/:network/:ip')
-  .get(dhcp.reservation_info);
+  .get(auth(true),dhcp.reservation_info);
 
 app.route(apipath+'/dhcp/:network/mac/:mac')
-  .get(dhcp.search_mac)
-  .delete(dhcp.delete_reservation);
+  .get(auth(true),dhcp.search_mac)
+  .delete(auth(true),dhcp.delete_reservation);
 
 app.route(apipath+'/dhcp/:network/ip/:ip')
-  .get(dhcp.search_ip)
+  .get(auth(true),dhcp.search_ip)
 
 app.route(apipath+'/dhcp/:network')
-  .post(dhcp.create_reservation);
+  .post(auth(true),dhcp.create_reservation);
 
 app.route(apipath+'/dns')
-  .post(dns.create);
+  .post(auth(true),dns.create);
 
 app.route(apipath+'/dns/:value')
-  .delete(dns.delete);
+  .delete(auth(true),dns.delete);
 
 app.route(apipath+'/dns/:value/A')
-  .delete(dns.deleteA);
+  .delete(auth(true),dns.deleteA);
 
 app.route(apipath+'/auto/:networkname/:value')
-  .put(dns.autocreate)
-  .get(dns.autocreate_noop)
-  .delete(dns.deleteA);
+  .put(auth(true),dns.autocreate)
+  .get(auth(true),dns.autocreate_noop)
+  .delete(auth(true),dns.deleteA);
 
 console.log("starting up....");
 try {
